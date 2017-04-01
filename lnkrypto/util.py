30bb6067d99c7069fe6767d97850834416dab737
liniku <develop.liniku@gmail.com>
2017-04-01 12:50:34 +0900

    support python3 only

--- lnkrypto/util.py
+++ lnkrypto/util.py
@@ -1,52 +1,49 @@
-import sys
+import binascii
 
-if sys.version_info[0] >= 3:
-    import binascii
+
+def int2bytes(i):
+    return i.to_bytes(max((i.bit_length() + 7) // 8, 1), byteorder='big')
+
+
+def bytes2int(b):
+    return int.from_bytes(b, byteorder='big')
+
+
+def str2bytes(s):
+    return s.encode('utf-8')
+
+
+def bytes2str(b):
+    return b.decode('utf-8')
+
+
+def hex2bytes(h):
+    return bytes.fromhex(h)
+
+
+def bytes2hex(b):
+    return bytes2str(binascii.hexlify(b))
 
 
 def int2hex(i):
-    if i < 0:
-        raise ValueError('Input integer must be not negative.')
-
-    h = hex(i)[2:].rstrip('L')
-    if len(h) % 2 == 1:
-        h = '0' + h
-    return h
+    return bytes2hex(int2bytes(i))
 
 
 def hex2int(h):
-    return int(h, 16)
+    return bytes2int(hex2bytes(h))
 
 
-if sys.version_info[0] >= 3:
-    def str2bytes(s):
-        return s.encode('utf-8')
+def hex2str(h):
+    return bytes2str(hex2bytes(h))
 
-    def bytes2str(b):
-        return b.decode('utf-8')
 
-    def hex2bytes(h):
-        return bytes.fromhex(h)
-
-    def bytes2hex(b):
-        return bytes2str(binascii.hexlify(b))
-
-    def hex2str(h):
-        return bytes2str(hex2bytes(h))
-
-    def str2hex(s):
-        return bytes2hex(str2bytes(s))
-else:
-    def hex2str(h):
-        return h.decode('hex')
-
-    def str2hex(s):
-        return s.encode('hex')
+def str2hex(s):
+    return bytes2hex(str2bytes(s))
 
 
 def int2str(i):
-    return hex2str(int2hex(i))
+    return bytes2str(int2bytes(i))
 
 
 def str2int(s):
-    return hex2int(str2hex(s))
+    return bytes2int(str2bytes(s))
