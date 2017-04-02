d82ccfd9a5e65f367aecc2ecb829e5c2bfc55f8c
liniku <develop.liniku@gmail.com>
2017-04-02 19:32:44 +0900

    add frequency attack to simple xor cipher

--- lnkrypto/util.py
+++ lnkrypto/util.py
@@ -1,5 +1,7 @@
 import binascii
 
+DEFAULT_SYMBOL_COVERAGE_THRES = 0.3
+
 
 def int2bytes(i, min_length=1):
     if min_length < 1:
@@ -52,8 +54,40 @@ def str2int(s):
     return bytes2int(str2bytes(s))
 
 
-def human_can_read(b):
+def printable_ascii(b):
     for c in b:
         if (c < 32 or c > 126) and c != 9 and c != 10:
             return False
     return True
+
+
+def human_can_read_en(b,
+                      symbol_coverage_thres=DEFAULT_SYMBOL_COVERAGE_THRES):
+    if not printable_ascii(b):
+        return False
+
+    # count [^ .-,0-9a-zA-Z]
+    s = 0
+    for c in b:
+        if c != 32 and (c < 44 or c > 46) \
+                and (c < 48 or c > 57) \
+                and (c < 65 or c > 90) \
+                and (c < 97 or c > 122):
+            s += 1
+    if (s / len(b)) >= symbol_coverage_thres:
+        return False
+    return True
+
+
+def hamming_distance(b1, b2):
+    if len(b1) != len(b2):
+        raise ValueError('Length of two bytes are different.')
+
+    r = 0
+    for c1, c2 in zip(b1, b2):
+        x = c1 ^ c2
+        while x > 0:
+            r += x & 1
+            x >>= 1
+
+    return r
